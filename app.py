20c4d19234d6b2743c478e11e85f216ca0717e7f
ad <adityaagrawal478@gmail.com>
2025-02-28 00:50:32 +0530

    port

--- app.py
+++ app.py
@@ -29,4 +29,5 @@ def elements():
     return render_template('elements.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port, debug=True)
